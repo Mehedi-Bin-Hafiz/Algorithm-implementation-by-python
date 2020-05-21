ac60b2ef4317ae7ff7dcc29e5d20ccfb10255a86
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-22 02:03:32 +0600

    Implementation of sieve algorithm.

--- Find prime by sieve of erotosthenes.py
+++ Find prime by sieve of erotosthenes.py
@@ -1,18 +1,16 @@
+nums=[x for x in range(2,101)]
+print(nums)
 
+print("\n\n")
+for l in range(0,12):
+    pivot = nums.pop(l)
+    for i in nums:
+        if i % pivot == 0:
+            nums.remove(i)
+    nums.insert(l,pivot)
 
-num=[x for x in range(0,100) if x>1]
-for l in range(0,11):
-    pivot=num.pop(l)
-    for i  in num:
-        if i%pivot==0:
-            num.remove(i)
-    num.insert(l,pivot)
-print(num)
-print("total prime numbers:",len(num))
-
-
-
-
+print(nums)
+print("The number of prime numbers: ",len(nums))
 
 
 
