0e4049e355693bca8c41cfa3782b17273b920d1e
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-26 23:41:30 +0600

    Binary search by python

---
+++ binary_search.py
@@ -0,0 +1,31 @@
+import datetime
+
+begin_time = datetime.datetime.now()
+l=[1,6,3,1,5,8,4,2,1]
+l.sort()
+
+key=8
+low=0
+high=len(l)-1
+while(low<=high):
+    mid=int((low+high)/2)
+    if (l[mid] == key):
+        print('found at index',mid)
+        break
+    elif(l[mid]>key):
+        high=mid-1
+    elif(l[mid]<key):
+        low=mid+1
+    else:
+        pass
+else:
+    print("not found")
+
+
+print(datetime.datetime.now() - begin_time)
+
+
+
+
+
+
