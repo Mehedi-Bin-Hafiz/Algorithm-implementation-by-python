a1ee98215616469025300e14bdc09af8663fd732
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-05-26 23:41:03 +0600

    Introduction to linear search and implementation by python

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,6 +1,6 @@
-
-nums=[2,3,5,7,11,13,13,19,23,29]
-
+import datetime
+begin_time = datetime.datetime.now()
+nums=[1,6,3,1,5,8,4,2,1]
 
 def linear(nums,key):
     global ind
@@ -17,3 +17,5 @@ key=int(input("Enter your key: "))
 res=linear(nums,key)
 
 print(key,"is ", res , " at ", ind)
+
+print(datetime.datetime.now() - begin_time)
